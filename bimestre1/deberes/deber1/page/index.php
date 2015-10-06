8d9866da26a24b9dbf600068830bbb04c037d039
ronny <veloz_ronny10@hotmail.com>
2015-10-06 15:47:55 -0500

    deber1

---
+++ bimestre1/deberes/deber1/page/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mundo Musica</title>
+    
+
+    
+<style>
+ul#menu {
+ background: #333;
+ width: 940px;
+ float:left;
+ padding: 5px;
+}
+   
+ul#menu li {
+ color: #fff;
+ float: left;
+ list-style: none;
+ margin: 0% 5%;
+}
+   
+ul#menu li:hover{
+ color: #aaa073;
+ cursor:pointer;
+}
+   
+ul#menu ul {
+ display: none;
+ position: absolute;
+ top: 49px;
+ background: #333;
+ color: #fff;
+ padding: 5px 0px 5px 5px;
+ margin: 0;
+ }
+   
+ul#menu ul li{
+ float: left;
+ color: #fff;
+ width:100%;
+ margin:2% 0%;
+}
+  
+  
+ul#menu ul li a{
+ color: #fff;
+}
+   
+ul#menu ul li a:hover{
+ color: #aaa073;
+ cursor:pointer;
+}
+   
+ul#menu li:hover ul ul,ul#menu li:hover ul ul ul,ul#menu li.iehover ul ul,ul#menu li.iehover ul ul ul {
+ display: none;
+ cursor:pointer;
+}
+   
+ul#menu li:hover ul,ul#menu ul li:hover ul,ul#menu ul ul li:hover ul,ul#menu li.iehover ul,ul#menu ul li.iehover ul,ul#menu ul ul li.iehover ul {
+ display: block;
+ cursor:pointer;
+}
+    
+    div{color:#FFFFFF;
+    }
+    
+    #musica{color:#DCE30E;
+    }
+    #contenido{color:#1D0EE3;
+    }
+</style>
+</head>
+
+    <body background="image/music.jpg ">
+        
+        
+       <div align="center"> 
+<ul id="menu" align="center">
+ <li>INICIO</li>
+ <li>NOSOTROS</li>
+ <li>NOTICIAS</li>
+ <li>MUSICA
+  <ul>
+    <li>NACIONAL</li>
+    <li>INTERNACIONAL</li>
+  </ul>
+ </li>
+ <li>LETRAS</li>
+    
+</ul>
+           <br>
+        </div>
+        <br>
+        <div id="musica">
+            
+        <h1>ECUAMUSICA</h1></div>
+        <br>
+        <div id="contenido"align="center">
+            <h3> Queremos que por Medio de nuestra Musica , Se obtenga un buen resultado para la rutina diaria.
+                <br>
+
+Que seamos muy reconcidos por tener la mejor calidad de musica.
+                <br>
+            Cuando piensas en música probablemente recuerdes ciertas canciones que causaron una respuesta emocional. 
+                <br>
+                También puedes recordar una melodía que escuchaste en la radio o en una película. </h3>
+        </div>
+   <br>
+        <div>
+            <table  width=300 align="center">
+
+<tr>
+<td width=100>
+    <img src="image/microfono.png">
+</td>
+
+<td width=100>
+    <img src="image/dj.png">
+</td>
+    
+
+<td width=100>
+    <img src="image/nota.png">
+</td>
+</tr>
+
+<tr>
+<td width=100>
+    Aqui podras encontrar las letras de tu musica favorita ,para que puedas cantar como un profecional
+</td>
+
+<td width=100>
+    Podras mezclar tu musica de biblioteca y ser parte importante como animador de las fiestas
+</td>
+
+<td width=100>
+    Encontraras notas de tus canciones favoritas para poderlas interpretar como todo un maestro
+</td>
+</tr>
+
+</table>
+
+        </div>
+        
+            
+</body>
+</html>
